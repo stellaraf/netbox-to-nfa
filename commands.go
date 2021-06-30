b3299ad7f18749a39c068c8c6056d481a5426e3a
thatmattlove <matt@stellar.tech>
2021-06-30 15:53:37 -0700

    Add CLI command `config` to show config variables

--- commands.go
+++ commands.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"sort"
 	"strings"
 
@@ -11,19 +12,74 @@ import (
 )
 
 type argT struct {
-	Help bool `cli:"h,help" usage:"show help"`
+	cli.Helper
 }
 
-var help = cli.HelpCommand("display help information")
-
-func (argv *argT) AutoHelp() bool {
-	return argv.Help
+type cfgT struct {
+	cli.Helper
+	ShowPasswords bool `cli:"p,show-passwords" usage:"Show unobscured passwords in output"`
 }
 
 var rootCmd = &cli.Command{
 	Desc: "Synchronize Netbox Prefixes with Noction NFA",
 	Argv: func() interface{} { return new(argT) },
 	Fn: func(ctx *cli.Context) error {
+		return nil
+	},
+}
+
+var cfgCmd = &cli.Command{
+	Name: "config",
+	Desc: "Get validated configuration variables",
+	Argv: func() interface{} { return new(cfgT) },
+	Fn: func(ctx *cli.Context) error {
+		args := ctx.Argv().(*cfgT)
+		c := ctx.Color()
+		vars := make(map[string]string)
+		plainVars := []string{
+			"NETBOX_URL",
+			"NETBOX_TOKEN",
+			"NETBOX_NFA_ROLE",
+			"NFA_URL",
+			"NFA_USERNAME",
+		}
+		secureVars := []string{
+			"NETBOX_TOKEN",
+			"NFA_PASSWORD",
+		}
+		excluded := nfa.GetExcluded()
+		for _, k := range plainVars {
+			v := util.GetEnv(k)
+			vars[c.Magenta(k)] = c.Green(c.Bold(v))
+		}
+		for _, k := range secureVars {
+			v := util.GetEnv(k)
+			if args.ShowPasswords {
+				vars[c.Magenta(k)] = c.Red(c.Bold(v))
+			} else {
+				o := strings.Repeat("*", len(v))
+				vars[c.Magenta(k)] = c.Blue(c.Bold(o))
+			}
+		}
+		excludedV := ""
+		for i, e := range excluded {
+			l := len(excluded) - 1
+			pl := len("NB2NFA_EXCLUDED_RANGES: ")
+			p := strings.Repeat(" ", pl)
+			if i == 0 {
+				excludedV += fmt.Sprintf("%s\n", c.Yellow(c.Bold(e)))
+			} else if i == l {
+				excludedV += fmt.Sprintf("\t%s%s", p, c.Yellow(c.Bold(e)))
+			} else {
+				excludedV += fmt.Sprintf("\t%s%s\n", p, c.Yellow(c.Bold(e)))
+			}
+		}
+		vars[c.Magenta("NB2NFA_EXCLUDED_RANGES")] = excludedV
+		ctx.String("\n")
+		for k, v := range vars {
+			ctx.String("\t%s: %s\n", k, v)
+		}
+		ctx.String("\n")
 		return nil
 	},
 }
